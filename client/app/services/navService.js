84692ec0945af02ca38cd48e4f9b9c3e006e268c
salahShalaan <salahshalaan74@gmail.com>
2025-03-14 10:09:25 +0200

    Update BASE_URL

--- client/app/services/navService.js
+++ client/app/services/navService.js
@@ -1,5 +1,6 @@
 
-const BASE_URL = 'http://localhost:8081';
+// const BASE_URL = 'http://localhost:8081';
+const BASE_URL = 'https://pleasing-dedication-production.up.railway.app';
 
 const api = {
   get: async (endpoint) => {
